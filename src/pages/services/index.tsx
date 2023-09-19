dbb4cfaa9821be5c22ed291ec5550cf59f8c440e
Hojiakbar <pythoncoderuz@gmail.com>
2023-09-19 15:14:29 +0500

    add sponsors

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,8 +1,49 @@
 import Contract from "@/components/contracts";
 import Features from "@/components/features";
+import { useMemo } from "react";
 import Container from "@/layout/container";
+import Sponsor1 from "@/assets/images/sponsor-1.png";
+import Sponsor2 from "@/assets/images/sponsor-2.png";
+import Sponsor3 from "@/assets/images/sponsor-3.png";
+import Sponsor4 from "@/assets/images/sponsor-4.png";
+import Sponsor5 from "@/assets/images/sponsor-5.png";
+import Sponsor6 from "@/assets/images/sponsor-6.png";
+import Sponsor7 from "@/assets/images/sponsor-7.png";
 
 const Services = () => {
+  const sponsors = useMemo(
+    () => [
+      {
+        id: 1,
+        img: Sponsor1,
+      },
+      {
+        id: 2,
+        img: Sponsor2,
+      },
+      {
+        id: 3,
+        img: Sponsor3,
+      },
+      {
+        id: 4,
+        img: Sponsor4,
+      },
+      {
+        id: 5,
+        img: Sponsor5,
+      },
+      {
+        id: 6,
+        img: Sponsor6,
+      },
+      {
+        id: 7,
+        img: Sponsor7,
+      },
+    ],
+    []
+  );
   const links = [
     {
       id: 1,
@@ -17,7 +58,7 @@ const Services = () => {
   return (
     <Container title="Services" linksList={links} headerTitle="Services">
       <Features />
-      <Contract />
+      <Contract sliders={sponsors} />
     </Container>
   );
 };
