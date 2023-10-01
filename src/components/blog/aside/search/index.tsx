a16e2af5fcb01b70fca3b5d651dea905d1ea8ca2
Hojiakbar <pythoncoderuz@gmail.com>
2023-10-01 11:44:05 +0500

    add search section to aside

--- src/components/blog/aside/search/index.tsx
+++ src/components/blog/aside/search/index.tsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import { SearchIcon } from "@/components/icons";
+import { useNavigate } from "react-router-dom";
+
 const BlogSearch = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
   return (
     <div className="blog-aside__wrapper blog-search">
       <div className="blog-aside__wrapper-header">
         <h3 className="blog-aside__wrapper-title">Search</h3>
+      </div>
+      <div className="blog-search__container">
+        <div className="blog-search__wrapper">
+          <input
+            className="blog-search__wrapper-input"
+            type="text"
+            placeholder="Search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button
+            className="blog-search__wrapper-btn"
+            onClick={() => navigate(`/blog?search=${search}`)}
+          >
+            <SearchIcon />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -341,6 +341,25 @@ export const DoubleRightIcon = ({ ...props }) => {
   );
 };
 
+export const SearchIcon = ({ ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+      />
+    </svg>
+  );
+};
+
 export const Icon = () => {
   return null;
 };
