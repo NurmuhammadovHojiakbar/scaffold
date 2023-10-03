987a9c39595b36f7f3475623cc022e1b0ca14467
Hojiakbar <pythoncoderuz@gmail.com>
2023-10-03 13:53:09 +0500

    add pagination to blog

--- src/components/blog/blog-list/index.tsx
+++ src/components/blog/blog-list/index.tsx
@@ -1,13 +1,19 @@
 import { useGetBlogsQuery } from "@/store/api/blogs";
-import { queryMaker } from "@/utils/query";
-import { useLocation } from "react-router-dom";
+import { queryMaker, queryStringify } from "@/utils/query";
+import { useLocation, useNavigate } from "react-router-dom";
 import BlogItem from "../blog-item";
+import Pagination from "@/components/pagination";
 
 const BlogList = () => {
   const { search: query } = useLocation();
+  const navigate = useNavigate();
   const queryObj = queryMaker(query);
   const { page, search, tag, category } = queryObj;
   const { data: blogs } = useGetBlogsQuery({ page, search, tag, category });
+
+  const pageChangeHandler = (page: number) => {
+    navigate(`/blog?${queryStringify(queryObj, { page })}`);
+  };
 
   return (
     <div className="blog-list">
@@ -16,6 +22,14 @@ const BlogList = () => {
           <BlogItem blog={blog} key={blog.id} />
         ))}
       </ul>
+      {(blogs?.count || 0) > 10 && (
+        <Pagination
+          currentPage={Number(page) || 1}
+          total={blogs?.count || 0}
+          itemsPerPage={10}
+          handler={pageChangeHandler}
+        />
+      )}
     </div>
   );
 };
